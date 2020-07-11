77275165dfce040f1d92ed5a3648be22f9f90244
Dalton-cmd <Dalton.t.hoekstra@gmail.com>
2020-07-11 14:48:26 -0500

    my first commit - basic dashboard functionality

---
+++ stockTracker.py
@@ -0,0 +1,79 @@
+# EXPAND STOCK SYMBOL INPUT TO PERMIT MULTIPLE STOCK SELECTION
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output, State
+#import pandas_datareader.data as web # requires v0.6.0 or later
+from datetime import datetime
+import pandas as pd
+import yfinance as yf
+import os
+print(os.getcwd())
+nsdq = pd.read_csv(r'C:\Users\Dalton\Desktop\Python Projects\Stock Dashboard\data\NASDAQcompanylist.csv')
+nsdq.set_index('Symbol', inplace=True)
+options = []
+for tic in nsdq.index:
+    options.append({'label':'{} {}'.format(tic,nsdq.loc[tic]['Name']), 'value':tic})
+
+app = dash.Dash()
+
+app.layout = html.Div([
+    html.H1('Stock Ticker Dashboard'),
+    html.Div([
+        html.H3('Select stock symbols:', style={'paddingRight':'30px'}),
+        dcc.Dropdown(
+            id='ticker_symbol',
+            options=options,
+            value=['TSLA'],
+            multi=True
+        )
+    ], style={'display':'inline-block', 'verticalAlign':'top', 'width':'30%'}),
+    html.Div([
+        html.H3('Select start and end dates:'),
+        dcc.DatePickerRange(
+            id='my_date_picker',
+            min_date_allowed=datetime(2015, 1, 1),
+            max_date_allowed=datetime.today(),
+            start_date=datetime(2020, 1, 1),
+            end_date=datetime.today()
+        )
+    ], style={'display':'inline-block'}),
+    html.Div([
+        html.Button(
+            id='submit-button',
+            n_clicks=0,
+            children='Submit',
+            style={'fontSize':24, 'marginLeft':'30px'}
+        ),
+    ], style={'display':'inline-block'}),
+    dcc.Graph(
+        id='my_graph',
+        figure={
+            'data': [
+                {'x': [1,2], 'y': [3,1]}
+            ]
+        }
+    )
+])
+@app.callback(
+    Output('my_graph', 'figure'),
+    [Input('submit-button', 'n_clicks')],
+    [State('ticker_symbol', 'value'),
+    State('my_date_picker', 'start_date'),
+    State('my_date_picker', 'end_date')])
+def refreshData(n_clicks, options, start_date, end_date):
+    start = datetime.strptime(start_date[:10], '%Y-%m-%d')
+    end = datetime.strptime(end_date[:10], '%Y-%m-%d')
+    traces = []
+    for tic in options:
+#        stockdf = pdr.get_data_yahoo(tic, start=start_date, end=end_date)
+        stockdf = yf.download(tic,  start=start, end=end)
+        traces.append({'x':stockdf.index, 'y':stockdf.Close,'name':tic})
+    fig = {
+        'data': traces,
+        'layout': {'title':', '.join(options)+' Closing Prices'}
+    }
+    return fig
+
+if __name__ == '__main__':
+    app.run_server()
